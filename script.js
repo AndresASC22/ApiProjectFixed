1f024d8c7fe004cc47873f19ef24f0c7c6eeb41f
AndresASC22 <109674299+AndresASC22@users.noreply.github.com>
2022-08-08 16:47:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// what if we used this as the api
+// https://api.chucknorris.io/jokes/random
+//https://api.chucknorris.io/ 
+
+//Shrikar, Brian, Andres 
+
+let button = document.querySelector('button');
+
+const BASE_URl = "https://api.chucknorris.io/jokes/random"
+
+let requestURL = `${BASE_URl}`;
+
+function displayFact() {
+
+    fetch(requestURL)
+
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let factObject = data.value;
+            console.log(factObject)
+
+            let caption = document.querySelector("p");
+            caption.innerHTML = factObject
+        })
+
+        .catch(function (error) {
+            console.log("Error during fetch:", error);
+        });
+
+
+}
+
+
+button.onclick = displayFact 
+
+
